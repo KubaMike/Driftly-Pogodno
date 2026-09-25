e93c564cc2deb4f64c01d9ee1f79a37e15747a64
KubaMike <jmstefaniak2011@gmail.com>
2026-09-25 14:10:58 +0200

    Increase map zoom to 14

--- src/data/dropPoints.ts
+++ src/data/dropPoints.ts
@@ -1,6 +1,6 @@
 import { pointSites } from '../points/registry';
 
 export const MAP_CENTER: [number, number] = [53.447, 14.536];
-export const MAP_ZOOM = 13;
+export const MAP_ZOOM = 14;
 
 export const dropPoints = pointSites;
